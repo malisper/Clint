94a25895287786fe3282a1ce950d3b6d75bb5025
Michael Malis <michaelmalis2@gmail.com>
2014-12-05 21:56:38 -0600

    Added support for dispatch macro characters.

--- src/reader.lisp
+++ src/reader.lisp
@@ -1,7 +1,8 @@
 (in-package :clint)
 
 (defclass cl-readtable ()
-  ((chars :initarg :chars :accessor readtable-chars :initform (make-hash-table))))
+  ((chars    :initarg :chars :accessor readtable-chars    :initform (make-hash-table))
+   (dispatch :initarg :chars :accessor readtable-dispatch :initform (make-hash-table))))
 
 (setf (global-var ^'*readtable*) (make-instance 'cl-readtable))
 
@@ -9,6 +10,25 @@
   "Has the reader call FN whenever CHAR is read."
   (declare (ignore non-terminating))
   (setf (gethash char (readtable-chars readtable)) fn)
+  t)
+
+(defun cl-make-dispatch-macro-character (char &optional non-terminating (readtable (global-var ^'*readtable*)))
+  "Create a dispatch-macro-character whose first character is CHAR."
+  (declare (ignore non-terminating))
+  (setf (gethash char (readtable-dispatch readtable)) (make-hash-table))
+  (cl-set-macro-character char
+    (lambda (stream c)
+      (declare (ignore c))
+      (let* ((next (read-char stream))
+             (fn (gethash next (gethash char (readtable-dispatch readtable))
+                          (lambda (stream c) (declare (ignore stream c))
+                            (error "Undefined dispatch-macro-character ~A~A" char next)))))
+        (funcall fn stream next)))
+    readtable))
+
+(defun cl-set-dispatch-macro-character (char subchar fn &optional (readtable (global-var ^'*readtable*)))
+  "Whenever the reader encounters CHAR followed by SUBCHAR, call FN."
+  (setf (gethash subchar (gethash char (readtable-dispatch readtable))) fn)
   t)
 
 (defun cl-get-macro-character (char &optional (readtable (global-var ^'*readtable*)))
@@ -28,21 +48,21 @@
   (declare (ignore recur-p))
   (loop with buffer = (adjustable-string)
         for char = (peek-char nil stream nil nil) do
-        (cond ((not char)
-               (if eof-error
-                   (error "EOF")
-                   (return eof-val)))
-              ((whitespace char)
-               (if (= (length buffer) 0)
-                   (read-char stream)
-                   (return (string->num/sym buffer))))
-              ((cl-get-macro-character char)
-               (if (= (length buffer) 0)
-                   (progn (read-char stream)
-                          (return (funcall (cl-get-macro-character char)
-                                           stream char)))
-                   (return (string->num/sym buffer))))
-              (:else (vector-push-extend (read-char stream) buffer)))))
+    (cond ((not char)
+           (if eof-error
+               (error "EOF")
+               (return eof-val)))
+          ((whitespace char)
+           (if (= (length buffer) 0)
+               (read-char stream)
+               (return (string->num/sym buffer))))
+          ((cl-get-macro-character char)
+           (if (= (length buffer) 0)
+               (progn (read-char stream)
+                      (return (funcall (cl-get-macro-character char)
+                                       stream char)))
+               (return (string->num/sym buffer))))
+          (:else (vector-push-extend (read-char stream) buffer)))))
 
 (defun string->num/sym (str)
   "Converts a string to either a number or a cl-symbol."
@@ -70,6 +90,11 @@
   (declare (ignore char))
   ^`(quote ,(cl-read stream)))
 
+(defun sharp-quote-reader (stream char)
+  "Reads in a sharp quote (#')."
+  (declare (ignore char))
+  ^`(function ,(cl-read stream)))
+
 (defun string-reader (stream char)
   "Reads in a string."
   (declare (ignore char))
@@ -91,6 +116,9 @@
 (cl-set-macro-character #\' 'quote-reader)
 (cl-set-macro-character #\" 'string-reader)
 (cl-set-macro-character #\; 'comment-reader)
+
+(cl-make-dispatch-macro-character #\#)
+(cl-set-dispatch-macro-character #\# #\' 'sharp-quote-reader)
 
 (defun eval-string (str)
   "Evaluate an expression from the given string."
