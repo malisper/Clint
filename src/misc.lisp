f96c71f8335b6b4ffea1300d812ee61a25e54c16
Michael Malis <michaelmalis2@gmail.com>
2014-12-19 18:51:59 -0600

    Forgot to add vars.lisp and misc.lisp.

---
+++ src/misc.lisp
@@ -0,0 +1,8 @@
+;;;; Miscellaneous ICL procedures.
+
+(in-package :clint)
+
+(defun cl-gensym (&optional (name "G"))
+  "Create a new Clint gensym with the given name."
+  (prog1 (cl-make-symbol (format nil "~A~A" name *cl-gensym-counter*))
+    (incf *cl-gensym-counter*)))
---
+++ src/vars.lisp
@@ -0,0 +1,14 @@
+;;;; Definition for variables that need to be accessible in both
+;;;; the ICL and Clint.
+
+(in-package :clint)
+
+(cl-defparameter *gensym-counter* *cl-gensym-counter* 0
+  "The number to be appended to the name of the next gensym.")
+(cl-defparameter t *cl-t* ^'t "The truth value.")
+
+;; There are some problems with making the Clint symbol nil and the
+;; empty list the same thing.
+
+;; (cl-defparameter nil *cl-nil* '() "The value of false and the empty list")
+
