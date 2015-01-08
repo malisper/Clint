4040716f1afb2018e10a4239b7ccd54b13678061
Michael Malis <michaelmalis2@gmail.com>
2015-01-07 19:09:23 -0600

    Added a way of determining if a variable is dynamically scoped.

--- src/core/syms.lisp
+++ src/core/syms.lisp
@@ -8,7 +8,9 @@
    (package :initarg :package :accessor cl-symbol-package :initform nil
             :documentation "The package this symbol belongs to.")
    (plist   :initarg :plist   :accessor cl-symbol-plist :initform nil
-	    :documentation "A symbol's plist.")))
+	    :documentation "A symbol's plist.")
+   (special :initarg :special :accessor cl-symbol-special :initform nil
+            :documentation "Is this variable dynamic?")))
 
 (defmethod print-object ((sym cl-symbol) s)
   "Prints the name of the Clint symbol."
