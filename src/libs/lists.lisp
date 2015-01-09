7eada89185688c090f4bae7e71bbaff922d3ceeb
Michael Malis <michaelmalis2@gmail.com>
2015-01-09 17:27:11 -0600

    Added append.

--- src/libs/lists.lisp
+++ src/libs/lists.lisp
@@ -21,3 +21,8 @@
   (if (null x)
       0
       (+ 1 (length (cdr x)))))
+
+(defun append (&rest xss)
+  (cond ((null (cdr xss)) (car xss))
+        ((null (car xss)) (apply #'append (cdr xss)))
+        ('else  (cons (caar xss) (apply #'append (cdar xss) xss)))))
