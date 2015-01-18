3eb9128982d2b8f75059c4f74ccd00b2bd40610a
Michael Malis <michaelmalis2@gmail.com>
2015-01-18 15:33:06 -0600

    Added external slot to symbols.
    This will be for packages to tell whether a symbol is internal or
    external for a given package.

--- src/core/syms.lisp
+++ src/core/syms.lisp
@@ -3,14 +3,16 @@
 (in-package :clint)
 
 (defclass cl-symbol ()
-  ((name    :initarg :name :accessor cl-symbol-name
-            :documentation "The name of this symbol.")
-   (package :initarg :package :accessor cl-symbol-package :initform nil
-            :documentation "The package this symbol belongs to.")
-   (plist   :initarg :plist   :accessor cl-symbol-plist :initform nil
-	    :documentation "A symbol's plist.")
-   (special :initarg :special :accessor cl-symbol-special :initform nil
-            :documentation "Is this variable dynamic?")))
+  ((name     :initarg :name :accessor cl-symbol-name
+             :documentation "The name of this symbol.")
+   (package  :initarg :package :accessor cl-symbol-package :initform nil
+             :documentation "The package this symbol belongs to.")
+   (plist    :initarg :plist   :accessor cl-symbol-plist :initform nil
+	     :documentation "A symbol's plist.")
+   (special  :initarg :special :accessor cl-symbol-special :initform nil
+             :documentation "Is this variable dynamic?")
+   (external :initarg :external :accessor cl-symbol-external :initform nil
+	     :documentation "Is this symbol external?")))
 
 (defmethod print-object ((sym cl-symbol) s)
   "Prints the name of the Clint symbol."
